fdc364a089f991cb36b11a06831e34841d30ce10
45h1m <ady.ashim@gmail.com>
2025-04-13 00:40:06 +0530

    for now filtering unique user vote by cookies

--- server/src/middlewares/checkFirstVisit.js
+++ server/src/middlewares/checkFirstVisit.js
@@ -1,41 +1,26 @@
-const visitorIPs = new Map();
-
-const normalizeIP = (ip) => {
-    // Check if IPv6 format like ::ffff:192.168.1.1
-    if (ip.startsWith('::ffff:')) {
-      // Extract the IPv4 part
-      return ip.substring(7);
-    }
-    return ip;
-  };
-  
-
 const checkFirstTimeVisitor = (req, res, next) => {
-    const userIP = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     const id = req.params.id;
-
-    console.log(userIP, id);
-
-    if (visitorIPs.has(userIP + id)) {
-
-        req.isFirstVisit = false;
-
-        const visitorData = visitorIPs.get(userIP + id);
-        visitorData.visits += 1;
-        visitorData.lastVisit = new Date().toISOString();
-        visitorIPs.set(userIP + id, visitorData);
+    
+    // Check if user has a visited cookie for this specific ID
+    const visitedPages = req.cookies.visitedPages || {};
+    
+    if (!visitedPages[id]) {
+      visitedPages[id] = true;
+      
+      res.cookie('visitedPages', visitedPages, {
+        maxAge: 30 * 24 * 60 * 60 * 1000, 
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production', 
+        sameSite: 'lax'
+      });
+      
+      req.isFirstVisit = true;
     } else {
-
-        req.isFirstVisit = true;
-        visitorIPs.set(userIP + id, {
-            firstVisit: new Date().toISOString(),
-            lastVisit: new Date().toISOString(),
-            visits: 1,
-        });
+      req.isFirstVisit = false;
     }
-
+    
     next();
-};
+  };
 
 
 module.exports = checkFirstTimeVisitor;
